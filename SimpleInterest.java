22604196f0638274a749d4d87633df0fea31b016
Mohammad Afzal Malik <mdafzalmalik13@gmail.com>
2025-07-03 23:31:46 +0530

    Add files via upload

---
+++ SimpleInterest.java
@@ -0,0 +1,15 @@
+package basic;
+
+public class SimpleInterest {
+
+	public static void main(String[] args) {
+		int p = 100;
+		int t = 20;
+		int r = 40;
+		int si = (p*r*t)/100;
+		System.out.println(si);
+
+
+	}
+
+}
