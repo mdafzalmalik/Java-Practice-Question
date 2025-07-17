e78dedf31229b251292ffb01fb32549e5464db92
Mohammad Afzal Malik <mdafzalmalik13@gmail.com>
2025-07-17 23:39:23 +0530

    Update SimpleInterest.java

--- SimpleInterest.java
+++ SimpleInterest.java
@@ -9,7 +9,7 @@ public class SimpleInterest {
 		int p = sc.nextInt();
 		int t = sc.nextInt();
 		int r = sc.nextInt();
-		int si = (p*r*t)/100;
+		int si = (p*r*t)/100;  // formula of SI ---> (SI = P X R X T) / 100
 		System.out.println(si);
 
 
